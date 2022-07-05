3c8fc2fd4408edba6cd5c9087a3d6ce6e0a9942d
ionShoot <if-19004@students.ithb.ac.id>
2022-07-05 15:25:32 +0700

    Menambahkan page tambah produk by rincian

---
+++ frontend/src/views/produk/TambahProdukbyRProyek.vue
@@ -0,0 +1,83 @@
+<template>
+    <v-card
+        class="mx-auto text-center mt-6"
+        max-width="1000">
+        <br>
+        <h1>Tambah Produk Baru Sesuai R.Proyek</h1>
+        <v-form
+            class="pa-6"
+            ref="form"
+            @submit.prevent="submitHandler"
+            v-model="valid"
+            lazy-validation
+        >
+            <v-text-field
+            v-model="id"
+            :counter="9"
+            :rules="idRules"
+            label="ID"
+            required
+            ></v-text-field>
+
+            <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            type="submit"
+            @click="InsertProduk()"
+            >
+            Submit
+            </v-btn>
+
+            <v-btn
+            color="error"
+            class="mr-4"
+            @click="reset"
+            >
+            Reset
+            </v-btn>
+        </v-form>
+        <v-snackbar top color="green" v-model="snackBar">
+            Insert Produk Sukses!
+        </v-snackbar>
+    </v-card>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data: () => ({
+      valid: true,  
+      id: '',
+      idRules: [
+        v => !!v || 'ID is required',
+        v => (v && v.length <= 9 && v.length >= 5) || 'ID must be 5-9 characters',
+      ],
+    }),
+
+    methods: {
+      reset () {
+        this.$refs.form.reset()
+      },
+
+      submitHandler() {
+        console.log(this.id)
+      },
+
+      async InsertProduk(){
+        try{
+          const response = await axios.post('/jproduct/post_jproductbyrproyek',
+            { id: this.id
+            }
+          );
+          console.log(response,this.data)
+          this.snackBar = true
+        }
+        catch(error){
+          alert("Insert Produk Failed")
+          console.log(error)
+        }
+      },
+    }
+  }
+</script>
