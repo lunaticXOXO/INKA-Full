c51c91eb97de96eb9ccac5dc6d59f5ce8ee47fe4
ionShoot <if-19004@students.ithb.ac.id>
2022-09-02 13:00:34 +0700

    Memperbaiki tampilan list operator hadir

--- frontend/src/views/operator/ListOperatorHadir.vue
+++ frontend/src/views/operator/ListOperatorHadir.vue
@@ -3,21 +3,28 @@
         <v-card class="mx-auto text-center mt-6 mb-6" max-width="1000" elevation="0">
             <h1>Operator Hadir</h1>
         </v-card>
-        <div class="d-flex mx-auto text-center" max-width="1000">
-            <v-card v-for="item in items" :key="item" class="mx-2" width="150">
-                <div class="">
-                    <v-img
-                        max-height="80"
-                        max-width="80" 
-                        src="../operator/contoh-profile.jpg"
-                        class="ma-6 ml-9">
-                    </v-img>
-                    <p>{{ item.title }}</p>
-                    <p>{{ item.name }}</p>
-                </div>
-            </v-card>
-        </div>
-        
+        <v-card class="mx-auto" max-width="1250" elevation="0">
+            <v-container fluid>
+                <v-row dense>
+                    <v-col
+                        v-for="item in items"
+                        :key="item"
+                        :cols="item.flex"
+                    >
+                        <v-card class="mx-auto justify-center text-center" elevation="0" outlined>
+                            <v-img
+                                max-height="80"
+                                max-width="80" 
+                                src="../operator/contoh-profile.jpg"
+                                class="ma-6 ml-14">
+                            </v-img>
+                            <p>{{ item.title }}</p>
+                            <p>{{ item.name }}</p>  
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </v-app>
 </template>
 
@@ -26,17 +33,17 @@ export default {
     data() {
         return {
             items: [
-                { title: '651100028', name: 'SAEPUDIN' },
-                { title: '999900105', name: 'PURYADI' },
-                { title: '651400111', name: 'WAHYUDI' },
-                { title: '651100023', name: 'MUHAMMAD AZIZ K' },
-                { title: '651600506', name: 'JEMY HERDIAN NUR' },
-                { title: '651400110', name: 'KUSANTOK'},
-                { title: '651600010', name: 'ERWIN DWI ANDRIYANTO'},
-                { title: '651600437', name: 'DANY WAHYU S'},
-                { title: '651600475', name: 'BAGAS LIDAKDHO'},
-                { title: '651500190', name: 'PUPUT ABDUL SAPUTRA'},
-            ]
+                { title: '651100028', name: 'SAEPUDIN', flex: 2 },
+                { title: '999900105', name: 'PURYADI', flex: 2 },
+                { title: '651400111', name: 'WAHYUDI', flex: 2 },
+                { title: '651100023', name: 'MUHAMMAD AZIZ K', flex: 2 },
+                { title: '651600506', name: 'JEMY HERDIAN NUR', flex: 2 },
+                { title: '651400110', name: 'KUSANTOK', flex: 2},
+                { title: '651600010', name: 'ERWIN DWI ANDRIYANTO', flex: 2},
+                { title: '651600437', name: 'DANY WAHYU S', flex: 2},
+                { title: '651600475', name: 'BAGAS LIDAKDHO', flex: 2},
+                { title: '651500190', name: 'PUPUT ABDUL SAPUTRA', flex: 2},
+            ],
         }
     }
 }
