6a9c8ae29d90f95b9ce8c12d598afbda3fc17816
fbbyfzh <fauziahfebby123@gmail.com>
2023-06-20 21:32:35 +0700

    test

--- backend/db/db_handler.py
+++ backend/db/db_handler.py
@@ -2,7 +2,7 @@ import mysql.connector
 def connector():
         connect = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
-                              database='risprosta')
+                              database='mapart')
         return connect
 
 
